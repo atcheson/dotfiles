7ee94e768cc6a0575cb735a94263a922b4ba8066
atcheson <m@atcheson.org>
2014-11-22 14:24:01 +0800

    now tracking colorschemes dir

---
+++ home/.colorschemes/vacuous.sh
@@ -0,0 +1,25 @@
+# vacuous
+
+background="#1d1f21"
+foreground="#c5c8c6"
+cursor="#c5c8c6"
+mouse_background=""
+mouse_foreground=""
+highlight=""
+border=""
+color0="#b5bd68" # black
+color1="#a54242" # red
+color2="#8c9440" # green
+color3="#de935f" # yellow
+color4="#5f819d" # blue
+color5="#85678f" # magenta
+color6="#5e8d87" # cyan
+color7="#707880" # white
+color8="#373b41" # brblack
+color9="#cc6666" # brred
+color10="#b5bd68" # brgreen
+color11="#f0c674" # bryellow
+color12="#81a2be" # brblue
+color13="#b294bb" # brmagenta
+color14="#8abeb7" # brcyan
+color15="#c5c8c6" # brwhite
