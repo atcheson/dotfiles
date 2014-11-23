04d881e39f6c16bdb877c7c90ae73d5ddb480043
atcheson <m@atcheson.org>
2014-11-23 12:41:20 +0800

    bootstrap.sh should now be able to install pkglist

--- setup/system/bootstrap.sh
+++ setup/system/bootstrap.sh
@@ -7,32 +7,34 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 #install all packages
-echo "bootstrap.sh: installing packages"
-curl https://raw.githubusercontent.com/atcheson/dotfiles/master/setup/system/pkglist | xargs apt-get --assume-yes install
+echo "`basename $0`: installing packages"
+curl https://raw.githubusercontent.com/atcheson/dotfiles/master/setup/system/pkglist | \
+    sed  -r 's/i (\S+)\s+.*/\1/' | \
+    xargs apt-get --assume-yes install
 
 #set up sudo and /etc/sudoers
 sudoers_line="%sudo         ALL=(ALL:ALL) NOPASSWD: ALL"
 if ! grep -q "^\%sudo\s*ALL\=(ALL\:ALL)\s*NOPASSWD:\s*ALL.*$" /etc/sudoers; then
     echo "setting up sudoers"
     if [-f "/etc/sudoers.tmp"]; then
-        echo "bootstrap.sh ERROR: /etc/sudoers is locked for editing, aborting"
+        echo "`basename $0` ERROR: /etc/sudoers is locked for editing, aborting"
         exit 1
     fi
     touch /etc/sudoers.tmp
-    cp /etc/sudoers /tmp/sudoers.tmp
-    echo $sudoers_line >> /tmp/sudoers.tmp
-    visudo -c -f /tmp/sudoers.tmp
+    tempfile=/tmp/sudoers.tmp
+    cp /etc/sudoers $tempfile
+    echo $sudoers_line >> $tempfile
+    visudo -c -f $tempfile
     if [ "$? " -eq "0" ]; then
-        cp /tmp/sudoers.tmp /etc/sudoers
+        cp $tempfile /etc/sudoers
     fi
     rm /etc/sudoers.tmp
 fi
 
 #configure a user, if supplied
 if [ ! -z $i ]; then
-    echo "bootstrap.sh: user $1 added to group sudo"
+    echo "`basename $0`: user $1 added to group sudo"
     useradd -G sudo $1
     echo "now configuring user $1"
     sudo -u $1 curl -L deb-user-setup.atcheson.org | sh
 fi
-
