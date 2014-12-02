19dcb649e78f6d8e9ac69de4393bdfbf58485d34
atcheson <m@atcheson.org>
2014-12-02 17:07:04 +0800

    typo

--- setup/user/init.sh
+++ setup/user/init.sh
@@ -1,7 +1,7 @@
 echo "configuring user $USER with home directory $HOME"
 cd $HOME
 gem install --user-install homesick
-export PATH=$PATH:/$HOME/.gem/ruby/2.1.0/bin/
+export PATH=$PATH:$HOME/.gem/ruby/2.1.0/bin/
 homesick clone atcheson/dotfiles
 homesick symlink dotfiles
 vim +PluginInstall +PluginUpdate +qall
